05413d885a741ff9bc102dbc1ab0fda048992322
clarissavazquez <clarissa.vazquez1@gmail.com>
2016-04-24 22:44:19 -0700

    These are the files the students will write their code in

---
+++ myFilter3.py
@@ -0,0 +1,11 @@
+from PIL import Image
+
+# This function will add a filter onto the image
+def filter3(im):
+    # TODO:
+    #   (1) Load the image's pixel data
+    #   (2) Get the width and height of the image
+    #   (3) Create a new Image
+    #   (4) Iterate through every pixel in the image and change its color value
+    #   (5) Save the new image you created in (3) as "new.png"
+    #   (6) Show the image
